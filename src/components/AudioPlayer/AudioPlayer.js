d38a676641b22be155ae04c86a0dfb5cc9f250bf
DimaGT <dimon200117@gmail.com>
2021-12-23 22:52:20 +0200

    added audio player

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -1,6 +1,47 @@
+import { CircularProgress } from "@material-ui/core";
+import { PauseRounded, PlayArrowRounded } from "@material-ui/icons";
 import React from "react";
+import { useState } from "react";
 import "./AudioPlayer.css";
 
-export default function AudioPlayer() {
-  return <>AudioPlayer</>;
+export default function AudioPlayer({
+  sender,
+  audioUrl,
+  id,
+  setAudioId,
+  audioId,
+}) {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMediaLoaded, setIsMediaLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isMetadataLoaded, setIsMetadataLoaded] = useState(false);
+  const [sliderValue, setSliderValue] = useState(0);
+  const [duration, setDuration] = useState("");
+  return (
+    <>
+      <div className={`audioplayer ${sender ? "" : "audioplayer__alt"}`}>
+        {!isMediaLoaded ? (
+          <CircularProgress />
+        ) : isPlaying ? (
+          <PauseRounded className="pause" />
+        ) : !isPlaying ? (
+          <PlayArrowRounded />
+        ) : null}
+        <div>
+          <span
+            style={{ width: `${sliderValue}%` }}
+            className="audioplayer__slider--played"
+          />
+          <input
+            type="range"
+            min={"1"}
+            max={"100"}
+            value={sliderValue}
+            className="audioplayer__slider"
+          />
+        </div>
+      </div>
+      <span className="chat__timestamp audioplayer__time">{duration}</span>
+    </>
+  );
 }
